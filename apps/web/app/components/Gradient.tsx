1f86748ca913a91e312b131085c5c0d16091e3d7
Raju-github-profile <rajukadel.github@gmail.com>
2023-12-25 21:46:15 +0545

    feat:added gradient

---
+++ apps/web/app/components/Gradient.tsx
@@ -0,0 +1,23 @@
+
+export async function Gradient({
+    conic,
+    className,
+    small,
+}: {
+    small?: boolean;
+    conic?: boolean;
+    className?: string;
+}): JSX.Element {
+    return (
+        <span
+            className={[
+                styles.gradient,
+                conic ? styles.glowConic : undefined,
+                small ? styles.gradientSmall : styles.gradientLarge,
+                className,
+            ]
+                .filter(Boolean)
+                .join(" ")}
+        />
+    );
+}
